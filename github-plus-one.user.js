df33a94f98d7b40329fde3ad0e9b3461f6f3c927
whitered <dmitry.zhelnin@gmail.com>
2011-12-22 05:05:01 +0400

    script creates metatags to provide better title, description and image for sharing. it turned out that google do not use metadata from the page in browser, but instead it loads the page by itself, so my local metadata generation is poinless. let it just stay here for no reason

--- github-plus-one.user.js
+++ github-plus-one.user.js
@@ -7,12 +7,18 @@
 // @exclude        https://github.com/
 // @exclude        http://github.com/*/*/*
 // @exclude        https://github.com/*/*/*
-// @version        0.2
+// @version        0.3
 // ==/UserScript==
 
 function getElementByXPath(expr, node)
 {
   return document.evaluate(expr, node, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+function hasClass(ele, cls)
+{
+  if(!ele) return false;
+  return ele.className ? ele.className.split(" ").indexOf(cls) >= 0 : false;
 }
 
 function insertPlusOne(node)
@@ -29,16 +35,50 @@ function insertPlusOne(node)
   document.head.appendChild(po);
 }
 
-var pagehead = getElementByXPath("//ul[@class='pagehead-actions']", document);
-if(pagehead)
+function setItemType(type)
 {
-  var nodes = pagehead.parentNode.childNodes;
-  for (var i = 0; i < nodes.length; i++)
+  var html = document.getElementsByTagName("html")[0];
+  html.setAttribute("itemscope", "itemscope");
+  html.setAttribute("itemtype", "http://schema.org/" + type);
+}
+
+function setMeta(name, value)
+{
+  var meta = document.createElement("meta");
+  meta.setAttribute("itemprop", name);
+  meta.setAttribute("content", value);
+  document.head.appendChild(meta);
+}
+
+function getText(node)
+{
+  return node.innerHTML.split("\n").join(" ").replace(/<[^>]+>/g, " ").replace(/\s+/g, " ");
+}
+
+var pagehead = getElementByXPath("//div[contains(@class, 'pagehead')]", document);
+var h1 = pagehead.getElementsByTagName("h1")[0];
+if(!h1) return;
+
+if(hasClass(pagehead, "repohead")) // repository
+{
+  setItemType("Product");
+  setMeta("name", getElementByXPath("//head/title", document).innerHTML);
+  setMeta("description", getElementByXPath("//head/meta[@name = 'description']", document).getAttribute("content"));
+  setMeta("image", "https://a248.e.akamai.net/assets.github.com/images/icons/public.png");
+  insertPlusOne(h1);
+}
+else if(hasClass(pagehead, "userpage"))
+{
+  if(getElementByXPath(".//span[@class = 'organization-bit']", h1))
   {
-    if(nodes[i].nodeType == 1)
-    {
-      insertPlusOne(nodes[i]);
-      break;
-    }
+    setItemType("Organization");
   }
+  else
+  {
+    setItemType("Person");
+  }
+  setMeta("name", getElementByXPath("//head/title", document).innerHTML);
+  setMeta("description", getText(getElementByXPath("//div[contains(@class, 'profilecols')]", document)));
+  setMeta("image", getElementByXPath(".//img", h1).getAttribute("src"));
+  insertPlusOne(h1);
 }
